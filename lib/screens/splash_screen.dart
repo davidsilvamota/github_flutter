3ea191fdcaa1989fad26ba488fd06dc97c9096dc
David Mota <83174541+davidsilvamota@users.noreply.github.com>
2022-11-10 21:49:55 -0300

    create project structure

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.cyan]),
+          ),
+          child: const Center(
+            child: Text('Github'),
+          )),
+    );
+  }
+}
