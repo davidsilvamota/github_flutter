f4b0efad902ed46edf1293a9fdb55afc147f64ca
David Mota <83174541+davidsilvamota@users.noreply.github.com>
2022-11-10 21:55:58 -0300

    add style to screen splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -8,10 +8,17 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [Colors.blue, Colors.cyan]),
+            gradient:
+                LinearGradient(colors: [Color(0xFF2B2D2E), Color(0xFF000000)]),
           ),
           child: const Center(
-            child: Text('Github'),
+            child: Text(
+              'Github',
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
           )),
     );
   }
